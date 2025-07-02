4ab9c62f6d91f5591015b93cd9945d6c65557e93
Stanislav Kozlov <llarean@ya.ru>
2025-07-02 11:18:03 +0300

    The Database class and tests for it

---
+++ Assets/Plugins/PlayerPrefsDatabase/Runtime/Scripts/Database.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace LLarean.PlayerPrefsDatabase.Runtime
+{
+    public class Database
+    { 
+        public static void Save<T>(string key, T data)
+        {
+            if (data == null)
+            {
+                PlayerPrefs.DeleteKey(key);
+                return;
+            }
+            
+            var json = JsonUtility.ToJson(data);
+            PlayerPrefs.SetString(key, json);
+        }
+
+        public static T Load<T>(string key) where T : new()
+        {
+            if (PlayerPrefs.HasKey(key))
+            {
+                string json = PlayerPrefs.GetString(key);
+                return JsonDataSerializer.Deserialize<T>(json);
+            }
+            return new T();
+        }
+
+        public static void Delete(string key)
+        {
+            PlayerPrefs.DeleteKey(key);
+        }
+    }
+}
---
+++ Assets/Plugins/PlayerPrefsDatabase/Tests/Runtime/DatabaseTests.cs
@@ -0,0 +1,59 @@
+﻿using LLarean.PlayerPrefsDatabase.Runtime;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace LLarean.PlayerPrefsDatabase.Tests.Runtime
+{
+    [TestFixture]
+    public class DatabaseTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+
+        public class TestData
+        {
+            public int value;
+        }
+
+        [Test]
+        public void Save_And_Load_WorksCorrectly()
+        {
+            var data = new TestData { value = 42 };
+            Database.Save("test_key", data);
+
+            var loaded = Database.Load<TestData>("test_key");
+            
+            Assert.AreEqual(42, loaded.value);
+        }
+
+        [Test]
+        public void Load_NonExistingKey_ReturnsNewInstance()
+        {
+            var loaded = Database.Load<TestData>("non_existing_key");
+            
+            Assert.IsNotNull(loaded);
+            Assert.AreEqual(0, loaded.value);
+        }
+
+        [Test]
+        public void Delete_RemovesKey()
+        {
+            var data = new TestData { value = 10 };
+            Database.Save("delete_key", data);
+            Database.Delete("delete_key");
+
+            Assert.IsFalse(PlayerPrefs.HasKey("delete_key"));
+        }
+
+        [Test]
+        public void Save_NullData_DoesNotSave()
+        {
+            Database.Save<TestData>("null_key", null);
+            
+            Assert.IsFalse(PlayerPrefs.HasKey("null_key"));
+        }
+    }
+}
